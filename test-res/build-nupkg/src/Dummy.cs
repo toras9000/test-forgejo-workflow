a647abf7f7455e55e607281187f9b8d4c31a28a5
toras9000 <toras9000@example.com>
2025-01-15 23:58:06 +0900

    いくつかのワークフロー

---
+++ test-res/build-nupkg/src/Dummy.cs
@@ -0,0 +1,14 @@
+﻿
+using System.Reflection;
+
+/// <summary>適当なクラス</summary>
+public class Dummy
+{
+    /// <summary>適当なテキストを取得する</summary>
+    /// <returns>適当なテキスト</returns>
+    public string GetText()
+    {
+        var asmVer = Assembly.GetExecutingAssembly().GetName().Version;
+        return $"Dummy Text v{asmVer}";
+    }
+}
